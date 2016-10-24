344d60ebcf29e33558393844cff800ce04a0e15c
Burak <Burak@141.19.157.84>
2016-10-24 14:38:04 +0200

    SImu und Futterstelle geändert

--- 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/Futterstelle.java
+++ 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/Futterstelle.java
@@ -1,8 +1,9 @@
 package tpe.oo.polymorphie;
 
-import tpe.oo.polymorphie.tiere.Affe;
-import tpe.oo.polymorphie.tiere.Giraffe;
-import tpe.oo.polymorphie.tiere.Gorilla;
+//import tpe.oo.polymorphie.tiere.Affe;
+//import tpe.oo.polymorphie.tiere.Giraffe;
+//import tpe.oo.polymorphie.tiere.Gorilla;
+import tpe.oo.polymorphie.tiere.ZooTier;
 
 /**
  * Fütterung der Tiere.
@@ -10,29 +11,38 @@ import tpe.oo.polymorphie.tiere.Gorilla;
 public class Futterstelle {
 
     /**
-     * Füttert den Affen.
-     * 
-     * @param affe Affe, der gefüttert werden soll. 
+     * Füttert das Zootier.
+     *
+     * @param zooTier Tier, das gefüttert werden soll.
      */
-    public void gibFutter(Affe affe) {
-        affe.fuettern();
+    public void gibFutter(ZooTier zooTier) {
+        zooTier.fuettern();
     }
-    
-    /**
-     * Füttert den Gorilla.
-     * 
-     * @param gorilla Gorilla, der gefüttert werden soll. 
-     */
-    public void gibFutter(Gorilla gorilla) {
-        gorilla.fuettern();
-    }
-    
-    /**
-     * Füttert die Giraffe.
-     * 
-     * @param giraffe Giraffe, die gefüttert werden soll. 
-     */
-    public void gibFutter(Giraffe giraffe) {
-        giraffe.fuettern();
-    }
+
+//    /**
+//     * Füttert den Affen.
+//     *
+//     * @param affe Affe, der gefüttert werden soll.
+//     */
+//    public void gibFutter(Affe affe) {
+//        affe.fuettern();
+//    }
+//
+//    /**
+//     * Füttert den Gorilla.
+//     *
+//     * @param gorilla Gorilla, der gefüttert werden soll.
+//     */
+//    public void gibFutter(Gorilla gorilla) {
+//        gorilla.fuettern();
+//    }
+//
+//    /**
+//     * Füttert die Giraffe.
+//     *
+//     * @param giraffe Giraffe, die gefüttert werden soll.
+//     */
+//    public void gibFutter(Giraffe giraffe) {
+//        giraffe.fuettern();
+//    }
 }
--- 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/ZooSimulation.java
+++ 04_objektorientierung/05_polymorphie/src/tpe/oo/polymorphie/ZooSimulation.java
@@ -3,6 +3,7 @@ package tpe.oo.polymorphie;
 import tpe.oo.polymorphie.tiere.Affe;
 import tpe.oo.polymorphie.tiere.Giraffe;
 import tpe.oo.polymorphie.tiere.Gorilla;
+import tpe.oo.polymorphie.tiere.ZooTier;
 
 /**
  * Simulationsklasse.
@@ -15,31 +16,51 @@ public final class ZooSimulation {
     private ZooSimulation() {
         // keine Objekte benötigt
     }
-    
+
     /**
      * Main-Methode.
-     * 
+     *
      * @param args Kommandozeilen-Argumente.
      */
     public static void main(String[] args) {
         Futterstelle futterstelle = new Futterstelle();
-        
+
+        ZooTier[] tiere = new ZooTier[3];
+
         Affe charlie = new Affe("Charlie");
         Gorilla buck = new Gorilla("Buck");
         Giraffe debbie = new Giraffe("Debbie");
-        
-        System.out.println(charlie);
-        System.out.println(buck);
-        System.out.println(debbie);
-        
+
+        tiere[0] = charlie;
+        tiere[1] = buck;
+        tiere[2] = debbie;
+
+        for (int i = 0; i < tiere.length; i++) {
+            System.out.println(tiere[i]);
+        }
+
         System.out.println("Fütterung...");
-        
-        futterstelle.gibFutter(charlie);
-        futterstelle.gibFutter(buck);
-        futterstelle.gibFutter(debbie);
-        
-        System.out.println(charlie);
-        System.out.println(buck);
-        System.out.println(debbie);
+
+        for (int i = 0; i < tiere.length; i++) {
+            futterstelle.gibFutter(tiere[i]);
+        }
+
+        for (int i = 0; i < tiere.length; i++) {
+            System.out.println(tiere[i]);
+        }
+
+//        System.out.println(charlie);
+//        System.out.println(buck);
+//        System.out.println(debbie);
+//
+//        System.out.println("Fütterung...");
+//
+//        futterstelle.gibFutter(charlie);
+//        futterstelle.gibFutter(buck);
+//        futterstelle.gibFutter(debbie);
+//
+//        System.out.println(charlie);
+//        System.out.println(buck);
+//        System.out.println(debbie);
     }
 }
